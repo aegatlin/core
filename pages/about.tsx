002aef58dfaa700f66587bbc37afa0573f3a7bb3
Austin Gatlin <austin@gatlin.io>
2022-10-14 15:25:04 +1100

    Update about page.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,52 @@
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { Card } from '../src/core/Card'
 import { Header } from '../src/core/Header'
+
+function CodeLines({ lines }) {
+  return (
+    <code className="space-y-2 rounded-2xl bg-gray-50 p-4">
+      {lines.map((l, i) => (
+        <pre key={l + i}>{l}</pre>
+      ))}
+    </code>
+  )
+}
+
+function LinkText({ href, text }) {
+  return (
+    <NextLink href={href}>
+      <a className="text-blue-500">{text}</a>
+    </NextLink>
+  )
+}
 
 export default function AboutPage() {
   return (
     <>
       <Header />
-      <div className="flex justify-center">
+      <div className="mt-16 flex justify-center">
         <Card>
-          <div className="flex flex-col space-y-4">
-            <p>This is an experiment in component reusability. More to come.</p>
+          <div className="flex max-w-lg flex-col space-y-4 p-8">
+            <p>
+              <LinkText href="https://github.com/aegatlin/core" text="Core" />{' '}
+              is an experiment in component reusability.
+            </p>
+            <p>
+              Experimental copying is available via{' '}
+              <LinkText
+                href="https://github.com/aegatlin/gin"
+                text="aegatlin/gin"
+              />
+              :
+            </p>
+            <CodeLines
+              lines={['npm i -g @aegatlin/gin', 'gin react core -h']}
+            />
             <p>
               This is a{' '}
-              <Link href="https://gatlin.io">
+              <NextLink href="https://gatlin.io">
                 <a className="text-blue-500">gatlin.io</a>
-              </Link>{' '}
+              </NextLink>{' '}
               project.
             </p>
           </div>
--- src/demo/DemoWrapper.tsx
+++ src/demo/DemoWrapper.tsx
@@ -1,10 +1,21 @@
+import { Github } from 'lucide-react'
+import Link from 'next/link'
 import { Card } from '../core/Card'
 
 export function DemoWrapper({ title, children }) {
   return (
     <Card>
       <div className="w-80 p-8">
-        <div className="mb-4 border-b pb-4 text-4xl">{title}</div>
+        <div className="mb-4 flex items-center justify-between border-b pb-4 text-4xl">
+          <h2>{title}</h2>
+          <Link
+            href={`https://github.com/aegatlin/core/blob/main/src/core/${title}.tsx`}
+          >
+            <a>
+              <Github />
+            </a>
+          </Link>
+        </div>
         {children}
       </div>
     </Card>
